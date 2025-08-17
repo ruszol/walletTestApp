f8bd8adc006fbb304c2ccd2ec8c7a38acb2b0c3c
ruszol <ruslanzolotnikov@ya.ru>
2025-08-17 13:19:27 +0300

    testApp

--- src/main/java/com/ruszol/wallettestapp/controller/WalletController.java
+++ src/main/java/com/ruszol/wallettestapp/controller/WalletController.java
@@ -5,7 +5,6 @@ import com.ruszol.wallettestapp.dto.WalletDto;
 import com.ruszol.wallettestapp.dto.IncomingDto;
 import com.ruszol.wallettestapp.service.WalletService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/ruszol/wallettestapp/entity/Wallet.java
+++ src/main/java/com/ruszol/wallettestapp/entity/Wallet.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+
 @Table(name = "wallet")
 public class Wallet {
 
